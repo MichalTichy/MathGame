c78b17240b6a02cfcc20f486f6d5c5bbc7f644bd
Zuzka <xcisar07@stud.fit.vutbr.cz>
2018-11-20 22:27:52 +0200

    camera deadzone

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,6 +6,12 @@ public class CameraFollow : MonoBehaviour {
     private Transform player;
     private Vector3 offset;
 
+
+	public bool bounds;
+
+	public Vector3 minCameraPosition;
+	public Vector3 maxCameraPosition;
+
     // Use this for initialization
 	void Start ()
 	{
@@ -16,7 +22,17 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-	    transform.position = player.position + offset;
+
+		transform.position = player.position + offset;
+
+		if (bounds) {
+		
+			transform.position = new Vector3 (Mathf.Clamp(transform.position.x, minCameraPosition.x, maxCameraPosition.x),
+				Mathf.Clamp(transform.position.y, minCameraPosition.y, maxCameraPosition.y),
+				Mathf.Clamp(transform.position.z, minCameraPosition.z, maxCameraPosition.z));
+
+		}
+	    
 
 	}
 }
