7eb11a0bae24f83963e2be70ab0be673b33c624b
PrasekMatej <prasek.matej@seznam.cz>
2018-12-12 19:53:29 +0200

    Added numeric code puzzle to attic.

---
+++ Assets/Scripts/AtticPuzzleArrowController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtticPuzzleArrowController : MonoBehaviour
+{
+    public AtticInteraction awardScript;
+    public int[] password = new int[3];
+    public Text[] textFields;
+    private int[] currentCode = {0, 0, 0};
+
+    public void Start()
+    {
+        UpdateText();
+    }
+    public void ArrowUp(int index)
+    {
+        if (currentCode[index] >= 9)
+        {
+            currentCode[index] = 0;
+        }
+        else
+        {
+            currentCode[index]++;
+        }
+
+        UpdateText();
+        if(VerifyCode())
+            awardScript.End();
+    }
+
+    public void ArrowDown(int index)
+    {
+        if (currentCode[index] <= 0)
+        {
+            currentCode[index] = 9;
+        }
+        else
+        {
+            currentCode[index]--;
+        }
+
+        UpdateText();
+        if (VerifyCode())
+            awardScript.End();
+    }
+
+    private void UpdateText()
+    {
+        for (int i = 0; i < textFields.Length; i++)
+        {
+            textFields[i].text = currentCode[i].ToString();
+        }
+    }
+
+    private bool VerifyCode()
+    {
+        for (int i = 0; i < password.Length; i++)
+        {
+            if (password[i] != currentCode[i])
+                return false;
+        }
+
+        return true;
+    }
+}
