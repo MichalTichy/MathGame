c9e0f4485f1df33de580f7c05b32e34d4b52e7fd
Michal Tichý <michal.tichy@riganti.cz>
2019-01-16 00:32:46 +0200

    Computer puzzle

---
+++ Assets/Scripts/ComputerInteraction.cs
@@ -0,0 +1,17 @@
+﻿public class ComputerInteraction : CharacterInteractionWithSceneSwitching
+{
+    public override void AwardPlayer()
+    {
+        
+    }
+
+    public override bool ArePostConditionsMet()
+    {
+        return false;
+    }
+
+    protected override void Setup()
+    {
+        
+    }
+}
