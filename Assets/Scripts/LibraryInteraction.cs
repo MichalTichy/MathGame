76fe656224c4ba13ea8c4834bb01de5568c4a2cc
Michal Tichý <michal.tichy@riganti.cz>
2019-01-15 03:01:26 +0200

    Library puzzle

--- Assets/Scripts/LibraryInteraction.cs
+++ Assets/Scripts/LibraryInteraction.cs
@@ -5,10 +5,26 @@ public class LibraryInteraction : CharacterInteractionWithSceneSwitching
 {
     [Header("Award")]
     public BoxCollider2D AwardCollider;
+
+    public DialogBubble Dialog;
     public override void AwardPlayer()
     {
         AwardCollider.enabled = false;
+        Dialog.ShowBubble();
         UnityEngine.Debug.Log("Library completed!");
+    }
+
+    public bool CorrectBookSelected = false;
+
+    public void CorrectBookFinished()
+    {
+        CorrectBookSelected = true;
+        End();
+    }
+
+    public override bool ArePostConditionsMet()
+    {
+        return base.ArePostConditionsMet() && CorrectBookSelected;
     }
 
     protected override void Setup()
