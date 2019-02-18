3b1676a94165b3437331a148ff7f4070b1ea3f14
PrasekMatej <prasek.matej@seznam.cz>
2019-02-18 16:45:27 +0100

    Added background music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip[] clips;
+
+    void Start()
+    {
+        StartCoroutine(Play());
+    }
+
+    IEnumerator Play()
+    {
+        foreach (AudioClip clip in clips)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length);
+        }
+        StartCoroutine(Play());
+    }
+}
